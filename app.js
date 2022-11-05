5e3e3acc90f3fb23f73be5f70e673278899f3f55
vorobjewa1999 <vorobjewa1999@gmail.com>
2022-11-05 14:25:37 +0300

    Создана система проверки возраста

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 console.log ("Привет")
 
-const age = 15
-if (age > 18) {
-    console.log ("Вам больше 18 лет");
+let age = '12dss';
+
+age = +age;
+if (!isNaN(+age)) {
+    if (age > 18) {
+        console.log ("Вам больше 18 лет");
+    } else if (age < 18) {
+        console.log ("Вам меньше 18 лет");
+    } else if (age===18) {
+        console.log ("Вам 18 лет");
+    } else {
+        console.log ("Укажите ваш возраст");
+    }
 } else {
-    console.log ("Вам меньше 18 лет");
+    console.log ("Введите корректные данные");
 }
+
 
