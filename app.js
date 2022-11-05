27356eb48d798007c6e5f949fa087240150b8fdd
vorobjewa1999 <vorobjewa1999@gmail.com>
2022-11-05 14:52:11 +0300

    Тернарный оператор

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-console.log ("Привет")
+const isAdmin = true;
 
-let age = '12dss';
-
-age = +age;
-if (!isNaN(+age)) {
-    if (age > 18) {
-        console.log ("Вам больше 18 лет");
-    } else if (age < 18) {
-        console.log ("Вам меньше 18 лет");
-    } else if (age===18) {
-        console.log ("Вам 18 лет");
-    } else {
-        console.log ("Укажите ваш возраст");
-    }
-} else {
-    console.log ("Введите корректные данные");
-}
-
-
+console.log('Добро пожаловать: ${isAdmin ? "Администратор" : "Пользователь"}');
